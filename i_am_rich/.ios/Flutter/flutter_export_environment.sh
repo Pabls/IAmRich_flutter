e8f312adc01008272cdeddd5cf6c03c2bf406472
PS <pabls@yandex.ru>
2020-02-29 21:49:57 +0300

    change IOS icons and migration to AndroidX

---
+++ i_am_rich/.ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/pavel/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/pavel/Документы/flutter-apps/IAmRich/i_am_rich"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
