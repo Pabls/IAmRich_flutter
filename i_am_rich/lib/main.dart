5c515e7240a6bc4ed2dd7dcab30d7701717f3c61
PS <pabls@yandex.ru>
2020-02-29 20:38:24 +0300

    init

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+    MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("I'm Rich"),
+            backgroundColor: Colors.blueGrey[900]
+          ),
+          backgroundColor: Colors.blueGrey,
+          body: Center(
+            child: Image(
+              image: AssetImage(''),
+            ),
+          ),
+        ),
+    )
+);
